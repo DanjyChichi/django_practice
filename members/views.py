a6bdffb25bf61b4d21ea702425a166ce41f1743a
DanjyChichi <td0413@daum.net>
2021-12-15 20:32:01 +0900

    멤버 목록 만들기

--- members/views.py
+++ members/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Member
 
-# Create your views here.
+
+def index(request):
+    members = Member.objects.all().order_by('-pk')
+
+    return render(
+        request,
+        'index.html',
+        {
+            'members' : members,
+        }
+    )
