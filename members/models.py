a5a03a17d03445d8d5d74f33ac47b475ceedf241
DanjyChichi <td0413@daum.net>
2021-12-15 19:59:11 +0900

    모델 생성

--- members/models.py
+++ members/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Member(models.Model):
+    name = models.CharField(max_length=7)
+    age = models.PositiveIntegerField(max_length=3)
+    birth = models.DateField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'[{self.pk}] {self.name} {self.email}'
